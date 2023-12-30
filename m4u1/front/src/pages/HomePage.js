8bd28ae0c8bd98cb453aba9c56c91cf3eb72ca47
MatPerSua <mo.perezsuarez@gmail.com>
2023-12-30 00:07:08 -0300

    Migración React

--- m4u1/front/src/pages/HomePage.js
+++ m4u1/front/src/pages/HomePage.js
@@ -3,7 +3,29 @@ import React from "react";
 const HomePage = (props) => {
 
     return (
-        <p>Hola soy el Home Page</p>
+        <main className="holder">
+            <div className="heading">
+      <h1>Sobre Nosotros</h1>
+      <p>Un grupo de amigos que decidieron comenzar a jugar Calabozos y Dragones para hacerle frente a la pandemia y
+        cuarentena obligatoria durante el año 2020, y despúes siguieron jugando adentrandose en la historia de sus
+        personajes y la historia que su Maestro del Calabozo les propone.</p>
+    </div>
+    <div className="container">
+      <section className="about">
+        <div className="about-image">
+          <img src="img/Retrato.png" alt=""/>
+        </div>
+        <div className="about-content">
+          <h2>
+            Los Stray Sharpies
+          </h2>
+          <p>Una Hechicera escapando de casa, una picara escapando de las consecuencias de sus acciones, una druida buscando descubrir un mundo que le era negado y un monje con dotes musicales tratando de ganarse la vida de una manera honesta, se cuentran en un viaje donde sus vidas se conectaran de la forma más inusual y epica.</p>
+          <a href="temporadas.html" className="read-more">Leer Más</a>
+        </div>
+      </section>
+    </div>
+
+        </main>
     )
 }
 export default HomePage;
--- m4u1/front/src/pages/PersonajesPage.js
+++ m4u1/front/src/pages/PersonajesPage.js
@@ -3,7 +3,77 @@ import React from "react";
 const PersonajesPage = (props) => {
 
     return (
-        <p>Hola nosotros somos los Personajes Page</p>
+        <main className="holder">
+            <div className="team-container">
+        <div className="personajes-container">
+          <div className="text">Conocé Al Grupo</div>
+        </div>
+        <div className="row">
+          
+          <div className="tarj-perfil">
+            <div className="profile-content">
+              <div className="profile-image">
+                <img className="img-card" src="/personajes/chandler.png" alt="Chandle Goodenough, Bardo Monje."/>
+              </div>
+              <div className="descripcion">
+                <h2>Chandler Goodenough</h2>
+                <p>Bardo Monje <span>Nivel 10</span></p>
+              </div>
+              <div className="btn-div">
+                <button className="btn"><i/>Ver más</button>
+              </div>
+            </div>
+          </div>
+          
+          <div className="tarj-perfil">
+            <div className="profile-content">
+              <div className="profile-image">
+                <img className="img-card" src="/personajes/damaia.png" alt=""/>
+              </div>
+              <div className="descripcion">
+                <h2>Damaia Stormless</h2>
+                <p>Rogue <span>Nivel 10</span></p>
+              </div>
+              <div className="btn-div">
+                <button className="btn"><i/>Ver más</button>
+              </div>
+            </div>
+          </div>
+          
+          
+          <div className="tarj-perfil">
+            <div className="profile-content">
+              <div className="profile-image">
+                <img className="img-card" src="/personajes/ember.png" alt=""/>
+              </div>
+              <div className="descripcion">
+                <h2>Ember Ink</h2>
+                <p>Druida <span>Nivel 10</span></p>
+              </div>
+              <div className="btn-div">
+                <button className="btn">Ver más</button>
+              </div>
+            </div>
+          </div>
+          
+          <div className="tarj-perfil">
+            <div className="profile-content">
+              <div className="profile-image">
+                <img className="img-card" src="/personajes/luthien.png" alt=""/>
+              </div>
+              <div className="descripcion">
+                <h2>Luthien Dadode</h2>
+                <p>Paladina, ex-Hechicera<span>Nivel 10</span></p>
+              </div>
+              <div className="btn-div">
+                <button className="btn">Ver más</button>
+              </div>
+            </div>
+          </div>
+          
+        </div>        
+      </div>
+    </main>
     )
 }
 export default PersonajesPage;
