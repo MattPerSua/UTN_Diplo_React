d775acf054531a1ec23812128a0942650424ff5b
MatPerSua <mo.perezsuarez@gmail.com>
2024-01-09 12:13:56 -0300

    post y get

--- m4u4/app.js
+++ m4u4/app.js
@@ -56,6 +56,11 @@ app.post('/ingresar', function (req, res)
   res.redirect('/');
 });
 
+app.get('/salir', function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
+})
+
 
 
 // catch 404 and forward to error handler
