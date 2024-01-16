96e6815259a2246793c8d4046a70e07ff11056e6
MatPerSua <mo.perezsuarez@gmail.com>
2024-01-16 18:47:37 -0300

    login

--- m4u1/back/models/usuariosModel.js
+++ m4u1/back/models/usuariosModel.js
@@ -1,15 +1,16 @@
 var pool = require('./bd'); //llamando datos BD
 var md5 = require('md5');
 
+// En usuariosModel.js
 async function getUserByUsernameAndPassword(user, password) {
     try {
-        var query = 'select * from usuarios where usuario = $usuario and password = $password limit 1';
+        var query = 'SELECT * FROM usuarios WHERE usuario = ? and password = ? limit 1';
         var rows = await pool.query(query, [user, md5(password)]);
         return rows[0];
     } catch (error) {
         console.log(error);
-
     }
 }
+
 
 module.exports = { getUserByUsernameAndPassword }
