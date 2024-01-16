dfe97d87ee04a370813a47a7638a6369ad7f15ea
MatPerSua <mo.perezsuarez@gmail.com>
2024-01-16 12:05:20 -0300

    insert, uptade, delete

--- m5u3/app.js
+++ m5u3/app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var pool = require('./models/bd')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+var id = 24;
+pool.query('delete from empleados where id_emp=?', [id]).then(function (resultados){
+  console.log(resultados);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
