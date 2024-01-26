2c7be355998fc9361c9e98a2413a332536c9fc6a
MatPerSua <mo.perezsuarez@gmail.com>
2024-01-26 14:56:18 -0300

    Agregado el Props validation

--- m4u1/front/src/components/novedades/NovedadItem.js
+++ m4u1/front/src/components/novedades/NovedadItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const NovedadItem = (props) => {
     const { title, subtitle, imagen, body } = props;
@@ -8,10 +9,17 @@ const NovedadItem = (props) => {
             <h1>{title}</h1>
             <h2>{subtitle}</h2>
             <img src={imagen} alt=''/>
-            <div dangerouslySetInnerHTML={{ __html: body}}/>
+            <div dangerouslySetInnerHTML={{ __html: body }} />
             <hr />
         </div>
     );
 }
+
+NovedadItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    imagen: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+};
 
 export default NovedadItem;
