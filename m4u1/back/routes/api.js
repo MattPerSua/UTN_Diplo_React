fa386b827f4ee57a2f3fd8b1da080fdf8e6e7f7c
MatPerSua <mo.perezsuarez@gmail.com>
2024-01-26 14:37:24 -0300

    Api

--- m4u1/back/routes/api.js
+++ m4u1/back/routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
 
-router.get('novedades', async function (req, res, next) {
+router.get('/novedades', async function (req, res, next) {
     let novedades = await novedadesModel.getNovedades();
 
     novedades = novedades.map(novedades => {
